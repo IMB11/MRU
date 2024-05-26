42d22f8e1e4c11aed56c28034fdab2337810f243
Calum <contact@mineblock11.dev>
2024-05-26 14:39:25 +0100

    chore: test stonecutter

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 stonecutter active "1.20.4" /* [SC] DO NOT EDIT */
 
 stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) {
-    group = "project"
+    group = "mod"
     ofTask("build")
 }
 
