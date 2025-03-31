b708e838c85b8e664649acdbd7dfbb741f5518da
Calum <hendersoncal117@gmail.com>
2025-03-31 15:35:43 +0100

    fix: neoforge

--- src/main/java/dev/imb11/mru/LoaderUtils.java
+++ src/main/java/dev/imb11/mru/LoaderUtils.java
@@ -6,9 +6,7 @@ public class LoaderUtils {
     public static Path getConfigPath(String modID, String configFileName, String configExtension) {
         /*? if fabric {*/
         return net.fabricmc.loader.api.FabricLoader.getInstance().getConfigDir().resolve(modID).resolve(configFileName + "." + configExtension);
-        /*?} elif forge {*/
-        /*return net.minecraftforge.fml.loading.FMLLoader.getGamePath().resolve("config").resolve(modID).resolve(configFileName + "." + configExtension);
-         *//*?} else {*/
+        /*?} else {*/
         /*return net.neoforged.fml.loading.FMLLoader.getGamePath().resolve("config").resolve(modID).resolve(configFileName + "." + configExtension);
          *//*?}*/
     }
@@ -16,9 +14,7 @@ public class LoaderUtils {
     public static boolean isModInstalled(String modid) {
         /*? if fabric {*/
         return net.fabricmc.loader.api.FabricLoader.getInstance().isModLoaded(modid);
-        /*?} elif forge {*/
-        /*return net.minecraftforge.fml.loading.FMLLoader.getLoadingModList().getModFileById(modid) != null;
-         *//*?} else {*/
+        /*?} else {*/
         /*return net.neoforged.fml.loading.FMLLoader.getLoadingModList().getModFileById(modid) != null;
          *//*?}*/
     }
@@ -26,9 +22,7 @@ public class LoaderUtils {
     public static Path getGameDir() {
         /*? if fabric {*/
         return net.fabricmc.loader.api.FabricLoader.getInstance().getGameDir();
-        /*?} elif forge {*/
-        /*return net.minecraftforge.fml.loading.FMLLoader.getGamePath();
-         *//*?} else {*/
+        /*?} else {*/
         /*return net.neoforged.fml.loading.FMLLoader.getGamePath();
          *//*?}*/
     }
@@ -36,9 +30,7 @@ public class LoaderUtils {
     public static Path getConfigFolder(String modID) {
         /*? if fabric {*/
         return net.fabricmc.loader.api.FabricLoader.getInstance().getGameDir().resolve("config").resolve(modID);
-        /*?} elif forge {*/
-        /*return net.minecraftforge.fml.loading.FMLLoader.getGamePath().resolve("config").resolve(modID);
-         *//*?} else {*/
+        /*?} else {*/
         /*return net.neoforged.fml.loading.FMLLoader.getGamePath().resolve("config").resolve(modID);
          *//*?}*/
     }
